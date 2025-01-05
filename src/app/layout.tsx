692b9b1ee69797fe3eb5aeff85365dd6855eedf7
Joseph Masu <60499662+JosephMasu@users.noreply.github.com>
2025-01-05 20:41:55 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata: Metadata = {
-//   title: "Create Next App",
 
 export default function RootLayout({
   children,
