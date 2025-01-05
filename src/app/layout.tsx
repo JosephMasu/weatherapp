1330a483f4aec17480c153a7ff84e53facf8c5ca
Joseph Masu <60499662+JosephMasu@users.noreply.github.com>
2025-01-05 20:42:24 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-
-
 const inter = Inter({ subsets: ["latin"] });
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
